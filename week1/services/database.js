25d722e24b99861f20800c32b0c266bca9409472
Jason van Spijk <jcrspijk@avans.nl>
2024-02-05 10:52:42 +0100

    week1 opdrachten

---
+++ week1/services/database.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGO_URL || "mongodb://localhost:27017";
+const dbName = process.env.DB_NAME || "mydb";
+
+const client = new MongoClient(uri);
+
+const db = client.db(dbName);
+
+module.exports = {
+    db: db,
+    client: client
+};
