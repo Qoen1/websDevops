f263edd7078a89085196f4c6361f0c57388d0b19
koen <k.verstappen1@student.avans.nl>
2024-03-15 20:36:50 +0100

    added basic services

---
+++ TargetImageService/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+const app = express();
+// mongoose.connect('mongodb://localhost:27017/expressJSTest',{ useNewUrlParser: true });
+
+var jsonParser = bodyParser.json()
+
+//routes
+
+// app.use('/rooms/:id/lines', jsonParser, require('./routes/Lines'));
+
+//end routes
+
+//error handler
+app.get('/', function(req, res){
+  res.send('feckin\' works!');
+});
+
+
+app.listen(3000);
