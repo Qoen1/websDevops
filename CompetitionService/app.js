eca5718dd00844e6b92f04e1eb6525659d3b991c
koen <k.verstappen1@student.avans.nl>
2024-04-05 18:33:59 +0200

    alle services aan prometheus toegevoegd

--- CompetitionService/app.js
+++ CompetitionService/app.js
@@ -2,9 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
+const promBundle = require('express-prom-bundle')
+const metrics_middleware = promBundle({
+  includePath: true,
+  includeStatusCode: true,
+  normalizePath: true,
+  promClient: {
+    collectDefaultMetrics: {}
+  }
+})
 
 const jsonParser = bodyParser.json()
-
+app.use(metrics_middleware)
 mongoose.connect('mongodb://mongo/expressJSTest',{ useNewUrlParser: true });
 
 //routes
