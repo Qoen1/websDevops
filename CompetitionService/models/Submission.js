a0d71b0edc4b5c9f45dd17569ef067728f8448cc
Jason van Spijk <jcr.vanspijk@student.avans.nl>
2024-04-07 01:36:46 +0200

    Retrieve list of submission
    http://localhost:3005/:id

--- CompetitionService/models/Submission.js
+++ CompetitionService/models/Submission.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const submissionSchema = new mongoose.Schema({
     targetImageId: { type: mongoose.Types.ObjectId },
+    competitionId: { type: mongoose.Types.ObjectId },
     imageId: { type: mongoose.Types.ObjectId, required: true },
     userId: { type: String, required: true },
     submittedAt: { type: Date, required: true },
