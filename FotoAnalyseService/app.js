1d5bf4f544aba9fb0aaec4d5f960d2eae66e0bdf
KoenVerstappen <k.verstappen1@student.avans.nl>
2024-04-07 19:37:43 +0200

    probleempjes na merge opgelost

--- FotoAnalyseService/app.js
+++ FotoAnalyseService/app.js
@@ -22,6 +22,6 @@ app.use(express.json());
 
 //end routes
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
